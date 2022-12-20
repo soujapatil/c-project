f1745874838ee18fadfdeca35158be70a6770506
soujapatil <121082120+soujapatil@users.noreply.github.com>
2022-12-20 12:08:48 +0530

    Update spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\nWelCome");
+  printf("\nsitpark institute");
+  add();
+}
+void add()
+{
+  int a,b,c;
+  printf("\nEnter a & b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\nAdd=",c);
 }
